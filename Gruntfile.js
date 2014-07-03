9b0fba90106ff7464858ea29cef4418f682cc19d
Nahuel Sotelo <nahuelsotelo@gmail.com>
2014-07-04 00:02:51 +0200

    compress images

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,6 +38,16 @@ module.exports = function(grunt) {
         },
 
         // IMAGES ======================
+        imagemin: {                                 // Task
+            dynamic: {                              // Another target
+                files: [{
+                    expand: true,                   // Enable dynamic expansion
+                    cwd: 'src/',                    // Src matches are relative to this path
+                    src: ['**/*.{png,jpg,gif}'],    // Actual patterns to match
+                    dest: 'dist/'                   // Destination path prefix
+                }]
+            }
+        },
         svgmin: {
             options: {
                 plugins: [
