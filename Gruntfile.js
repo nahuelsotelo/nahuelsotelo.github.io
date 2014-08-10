1d3ea069c06ea901af9b161c8daf267ccbd370d2
Nahuel Sotelo <nahuelsotelo@gmail.com>
2014-08-11 01:01:03 +0200

    add Gemfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,7 @@ module.exports = function(grunt) {
         compass: {
             dist: {
                 options: {
+                    bundleExec: true,
                     sassDir: 'scss',
                     cssDir:  'css',
                     imagesDir: 'img',
